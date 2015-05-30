59ff128e86bc85fbdcbf5d660012404c1d8564b7
rubcuadra <ru_cuadra_e_96@hotmai.com>
2015-05-29 21:52:25 -0500

    Get post VenueDetail

--- venues/models.py
+++ venues/models.py
@@ -1,24 +1,29 @@
 from django.db import models
 
 # Create your models here.
+#blank=True,null=True volverlos NOT OBLIGATORY
+class User(models.Model):
+    faceBookId = models.CharField(max_length=200,verbose_name=u'FaceBookId')
+    name = models.CharField(max_length=200,verbose_name=u'Nombre')
+    def __unicode__(self):
+        return self.name
 
-class Categories(models.Model):
+class Category(models.Model):
     category = models.CharField(max_length=50,blank=True,null=True,verbose_name=u'Categoria')
-
     def __unicode__(self):
         return self.category
 
-class Cities(models.Model):
+class City(models.Model):
     city = models.CharField(max_length=25,blank=True,null=True,verbose_name=u'Ciudad')
     def __unicode__(self):
         return self.city
 
-class States(models.Model):
+class State(models.Model):
     state = models.CharField(max_length=25,blank=True,null=True,verbose_name=u'Estado')
     def __unicode__(self):
         return self.state
 
-class Venues(models.Model):
+class Venue(models.Model):
     fourSquareId = models.CharField(max_length=30,verbose_name=u'FourSquareId')
     name = models.CharField(max_length=45,verbose_name=u'Nombre')
     description = models.CharField(max_length=250,blank=True,null=True,verbose_name=u'Descripcion')
@@ -29,10 +34,27 @@ class Venues(models.Model):
     lat = models.CharField(max_length=25,verbose_name=u'Latitud')
     lng = models.CharField(max_length=25,verbose_name=u'Longitud')
     
-    category = models.ForeignKey(Categories,blank=True,null=True)
-    city = models.ForeignKey(Cities,blank=True,null=True)
-    state = models.ForeignKey(States,blank=True,null=True)
+    category = models.ForeignKey(Category,blank=True,null=True)
+    city = models.ForeignKey(City,blank=True,null=True)
+    state = models.ForeignKey(State,blank=True,null=True)
+    visits = models.IntegerField(max_length=10,default=0)
+    nowHere = models.IntegerField(max_length=10,default=0)
 
     def __unicode__(self):
         return self.fourSquareId + ' ' + self.name
 
+class WantToGo(models.Model):
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    venue = models.ForeignKey(Venue)
+    
+    def __unicode__(self):
+        return self.date + ' ' + self.user + ' a '+self.venue 
+
+class NowHere(models.Model):
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    venue = models.ForeignKey(Venue)
+
+    def __unicode__(self):
+        return self.date + ' ' + self.user + ' esta en '+self.venue
