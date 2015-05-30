8cf59a4d97ce6ab97a29ca43a56fc8c898954448
rubcuadra <ru_cuadra_e_96@hotmai.com>
2015-05-29 21:51:36 -0500

    get VenueDetail

--- venues/admin.py
+++ venues/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Venue)
+admin.site.register(models.Category)
+admin.site.register(models.City)
+admin.site.register(models.State)
+
+admin.site.register(models.User)
+admin.site.register(models.WantToGo)
+admin.site.register(models.NowHere)
+
+
