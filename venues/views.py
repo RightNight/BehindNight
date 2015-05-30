fe08daf5dd2279b4887b1e9ccd5339b8a962b25d
rubcuadra <ru_cuadra_e_96@hotmai.com>
2015-05-29 21:52:15 -0500

    Get post VenueDetail

--- venues/views.py
+++ venues/views.py
@@ -20,4 +20,13 @@ class VenuesView(APIView):
 	    model = models.Venue.objects.all()
 	    serializer_class = serializers.VenueSerializer(model, many="True")
 	    return Response(serializer_class.data)
-    	
+
+class VenueDetailView(APIView): #COMO EXPLICAR QUE PASARA UN PRIMARY KEY A ESTA CLASE??
+	def get(self, request, *args, **kwargs):
+	    model = models.Venue.objects.get(fourSquareId = self.kwargs['fSId'])
+	    serializer_class = serializers.VenueSerializer(model)
+	    return Response(serializer_class.data) 
+	def post(self, request, *args, **kwargs):
+		#Aqui es para modificar los nowHere, Visits
+
+		return null   	
